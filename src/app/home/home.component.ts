88c36ac2e2b95c2e72a1e096c69c87e94f6852ad
Jasminy <jasminyduuarte_@hotmail.com>
2017-11-22 10:24:23 -0400

    antes de atualizar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
@@ -11,20 +12,31 @@ export class HomeComponent implements OnInit {
   lista3: string[];
   lista4: string[];
   show = true;
+  assustos= [];
 
   constructor() {
   }
 
   ngOnInit() {
-    /*lista do rodapé */
+    // lista do rodapé
     this.lista1 = ['Quem somos', 'Pacotes', 'Termos', 'Pacotes', 'Suporte', 'Comentários', 'TicketPhone na mídia', 'Apoio a TickePhone'];
     this.lista2 = ['Checklista de tarefas', 'Lista de inscritos', 'Registro de presença e check-in', 'Contato de prestadores de serviços', 'Materias para dowloand', 'Certificados Digitais',
       'Canal da comunicação', 'Lojinha virtual', 'Sorteios', 'Pesquisas', 'Contagem de público', 'Letreiro virtual'];
     this.lista3 = ['Vendas on-line e off-line', 'Vendas pelo site e aplicativos', 'Vendas as individuais e multiplas', 'Vendas com lugar marcado', 'Vendas de produtos e servições', 'Vendas de pacotes'];
     this.lista4 = ['TicketPhone web', 'Aplicativo Organizador', 'Aplicativo Participante', 'Maquininhas TicketPhone'];
+    // select lista
+    this.assustos = [
+      {name: 'Ingressos'},
+      {name: 'Certificados'},
+      {name: 'Aplicativos'},
+      {name: 'Acesso a eventos'},
+      {name: 'Serviços'},
+      {name: 'Pacotes de serviços'},
+      {name: 'Outros'},
+    ];
   }
 
-  Mostra(){
+  Mostra() {
     this.show = !this.show;
 }
 
