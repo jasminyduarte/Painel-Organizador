c98d80a14b390037df9829b02067a1fd7b574b58
jasminy <jasminyduuarte_@hotmail.com>
2017-11-22 06:35:08 -0400

    ultimas modfificalçoes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
@@ -11,14 +10,23 @@ export class HomeComponent implements OnInit {
   lista2: string[];
   lista3: string[];
   lista4: string[];
- constructor() { }
+  show = true;
+
+  constructor() {
+  }
 
   ngOnInit() {
     /*lista do rodapé */
     this.lista1 = ['Quem somos', 'Pacotes', 'Termos', 'Pacotes', 'Suporte', 'Comentários', 'TicketPhone na mídia', 'Apoio a TickePhone'];
     this.lista2 = ['Checklista de tarefas', 'Lista de inscritos', 'Registro de presença e check-in', 'Contato de prestadores de serviços', 'Materias para dowloand', 'Certificados Digitais',
-      'Canal da comunicação', 'Lojinha virtual' , 'Sorteios', 'Pesquisas', 'Contagem de público', 'Letreiro virtual'];
+      'Canal da comunicação', 'Lojinha virtual', 'Sorteios', 'Pesquisas', 'Contagem de público', 'Letreiro virtual'];
     this.lista3 = ['Vendas on-line e off-line', 'Vendas pelo site e aplicativos', 'Vendas as individuais e multiplas', 'Vendas com lugar marcado', 'Vendas de produtos e servições', 'Vendas de pacotes'];
     this.lista4 = ['TicketPhone web', 'Aplicativo Organizador', 'Aplicativo Participante', 'Maquininhas TicketPhone'];
- }
+  }
+
+  Mostra(){
+    this.show = !this.show;
+}
+
+
 }
